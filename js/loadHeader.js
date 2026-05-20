8dee3b1281ae4d83185d783e37ac4c142afc7b4e
DCMetroMaker <dcmetromaker@gmail.com>
2026-05-20 19:29:02 -0400

    Update loadHeader.js

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -6,7 +6,7 @@ if (path.includes("/rolling-stock/")) {
     depth = "../";
 }
 
-fetch(depth + "components/header.html")
+fetch(depth + "../components/header.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("header-placeholder").innerHTML = data;
